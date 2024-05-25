07ee78422c64314aba6ec12b0ab0d8caa9808c60
avenbreaks <dev-cosmware@gitshock.com>
2024-05-25 13:00:27 +0700

    new vue commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+  import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/vue'
+
+  const projectId = 'bc3b39325dd604f219c9a5a86446bee1'
+  
+  // 2. Set chains
+  const cybria = {
+    chainId: 6661,
+    name: 'Cybria Mainnet',
+    currency: 'CYBA',
+    explorerUrl: 'https://cybascan.io',
+    rpcUrl: 'https://rpc-mainnet.cybria.io'
+  }
+
+  // 3. Create your application's metadata object
+  const metadata = {
+    name: 'Wallet Connect Setup',
+    description: 'WalletConnect For Cybria Network',
+    url: 'https://cybria.io', // url must match your domain & subdomain
+    icons: ['https://raw.githubusercontent.com/Cybria-Chain/frontend-configs/main/configs/network-icons/cybria.svg']
+  }
+
+  // 4. Create Ethers config
+  const ethersConfig = defaultConfig({
+    /*Required*/
+    metadata,
+
+    /*Optional*/
+    enableEIP6963: true, // true by default
+    enableInjected: true, // true by default
+    enableCoinbase: true, // true by default
+    rpcUrl: '...', // used for the Coinbase SDK
+    defaultChainId: 6661,
+  })
+
+  // 5. Create a Web3Modal instance
+  const modal = createWeb3Modal({
+    ethersConfig,
+    chains: [cybria],
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableOnramp: true, // Optional - false as default
+    allowUnsupportedChain: true,
+    tokens: {
+    6661: {
+      address: '0x95622Fce49d65D1101f6FDa8b6325459A6188E52',
+      image: 'https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=032' //optional
+    }
+  }
+  })
+</script>
+
+<template>
+  <w3m-button />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<script setup lang="ts">
+  import { useWeb3Modal } from '@web3modal/ethers/vue'
+  // 4. Use modal composable
+  const modal = useWeb3Modal()
+</script>
+
+<template>
+  <button @click="modal.open()">Open Connect Modal</button>
+  <button @click="modal.open({ view: 'Networks' })">Open Network Modal</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
